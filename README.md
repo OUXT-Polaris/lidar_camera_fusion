4c80d49b4762e2e01c9da5e04d119efc88f21a13
Masaya Kataoka <ms.kataoka@gmail.com>
2022-09-27 22:33:58 +0900

    update README
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
-# rclcpp_components_template
+# lidar_camera_fusion
 
-## How to use
-1. Replace all `pkgname` in the filename with the name you want to use.
-2. In CMakeLists.txt and packages.xml, modify these lines as you like.
-https://github.com/OUXT-Polaris/rclcpp_components_template/blob/f611c5997496086f2128c7ccab20e72fac79a05a/CMakeLists.txt#L2-L3
-https://github.com/OUXT-Polaris/rclcpp_components_template/blob/f611c5997496086f2128c7ccab20e72fac79a05a/package.xml#L4-L8
+## What is it?
+Lidar camera detection result fusion package using hungarian-algorithm.  
+We use implementation of [this repository](https://github.com/mcximing/hungarian-algorithm-cpp), thank you for [Cong Ma](https://github.com/mcximing)
--- include/lidar_camera_fusion/hungarian.hpp
+++ include/lidar_camera_fusion/hungarian.hpp
@@ -1,13 +1,34 @@
-///////////////////////////////////////////////////////////////////////////////
-// Hungarian.h: Header file for Class HungarianAlgorithm.
+// Copyright (c) 2016 Cong Ma
+// All rights reserved.
 //
-// This is a C++ wrapper with slight modification of a hungarian algorithm implementation by Markus Buehren.
-// The original implementation is a few mex-functions for use in MATLAB, found here:
-// http://www.mathworks.com/matlabcentral/fileexchange/6543-functions-for-the-rectangular-assignment-problem
+// Software License Agreement (BSD License 2.0)
 //
-// Both this code and the orignal code are published under the BSD license.
-// by Cong Ma, 2016
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
 //
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+//  * Redistributions in binary form must reproduce the above
+//    copyright notice, this list of conditions and the following
+//    disclaimer in the documentation and/or other materials provided
+//    with the distribution.
+//  * Neither the name of the copyright holder nor the names of its
+//    contributors may be used to endorse or promote products derived
+//    from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+// ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
 
 #ifndef HUNGARIAN_H
 #define HUNGARIAN_H
